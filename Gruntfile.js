e17ad4856e7f13fa60b1c1968335490207d4ffe8
Ronald Gee <ron@august.com>
2015-11-27 20:56:41 -0800

    stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
+  var pkg = require('./package.json');
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
@@ -380,6 +381,34 @@ module.exports = function (grunt) {
         'imagemin',
         'svgmin'
       ]
+    },
+    
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      development: {
+        options: {
+          remote: 'git@github.com:rjgee/temp.git',
+          branch: 'master'
+        }
+      },
+      production: {
+        options: {
+          remote: 'git@github.com:rjgee/temp.git',
+          branch: 'master',
+          tag: pkg.version
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
     }
   });
 
@@ -441,4 +470,6 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+  
+  grunt.loadNpmTasks('grunt-build-control');
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
